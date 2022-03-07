fa878f4c6d467911e31deea05d31b59a0c33570a
Ednildo <ednildocunha@gmail.com>
2022-03-07 09:56:53 -0300

    project created

---
+++ src/main/java/com/edestudos/edestudos/EdestudosApplication.java
@@ -0,0 +1,13 @@
+package com.edestudos.edestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdestudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdestudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edestudos/edestudos/EdestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edestudos.edestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
